b40ff8f3ab06a8ef150eff642ce74a35baa2b0c2
theod <theodor.ciobanoiu@gmail.com>
2022-09-12 11:33:19 +0300

    Finished first part of the app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import {useForm} from "react-hook-form";
+import {useState} from "react";
+import ToDo from "./components/ToDo";
+const App = () => {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [toDoData, setData] = useState([]);
+    const { register, handleSubmit } = useForm();
+
+    const deleteToDo = (toDoDelete) => {
+        setData(toDoData.filter((item) => (item!== toDoDelete)));
+    }
+
+    const onSubmit = (data, e) => {
+        console.log(data, e);
+        setData([...toDoData, data['to-do']]);
+        console.log(toDoData);
+    };
+
+    const onError = (errors, e) => console.log(errors, e);
+
+    return (
+        <div className="App">
+
+            <form onSubmit={handleSubmit(onSubmit, onError)}>
+                <label>
+                    To do:
+                    <input {...register("to-do")} style={{marginLeft: 10}}/>
+                </label>
+                <input type="submit" value="Submit"  style={{marginLeft: 10}}/>
+            </form>
+            <ul>
+                {toDoData.map((item, index) => {
+                return <li key={index}><ToDo todo={item} deleteToDo={deleteToDo}/></li>
+            })}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/ToDo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ToDo = (props) => {
+    return (
+        <div className="toDoComponent">
+            <div>{props.todo}</div>
+            <button onClick={() => {
+                props.deleteToDo(props.todo)
+            }}>delete
+            </button>
+        </div>
+    );
+}
+
+export default ToDo;
