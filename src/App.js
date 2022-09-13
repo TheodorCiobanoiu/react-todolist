5236459f89c791c761dd76004bf5639b20abe18a
theod <theodor.ciobanoiu@gmail.com>
2022-09-13 11:56:49 +0300

    Integration with back-end

--- src/App.js
+++ src/App.js
@@ -8,13 +8,36 @@ const App = () => {
     const { register, handleSubmit } = useForm();
 
     const deleteToDo = (toDoDelete) => {
-        setData(toDoData.filter((item) => (item!== toDoDelete)));
+        setData(toDoData.filter((item) => (item !== toDoDelete)));
+        //TODO: Make 'DELETE' request to back-end to delete ToDo
+        fetch('http://localhost:8090/todo/delete/' + toDoDelete.id,{method: 'DELETE'}).then();
+        syncWithServer();
     }
 
     const onSubmit = (data, e) => {
+        console.log("DATA OBJECT")
         console.log(data, e);
-        setData([...toDoData, data['to-do']]);
         console.log(toDoData);
+        //TODO: Make 'POST' request to back-end to save new ToDo
+        fetch('http://localhost:8090/todo/save',{
+            method:'POST',
+            body: JSON.stringify(data),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }).then();
+        syncWithServer();
+    };
+
+    //TODO: Make 'GET' method to get all ToDo's from back-end
+    const syncWithServer = () =>{
+        fetch('http://localhost:8090/todo/get')
+            .then(response => response.json())
+            .then(result => {
+                setData(result);
+            });
+
     };
 
     const onError = (errors, e) => console.log(errors, e);
@@ -25,13 +48,13 @@ const App = () => {
             <form onSubmit={handleSubmit(onSubmit, onError)}>
                 <label>
                     To do:
-                    <input {...register("to-do")} style={{marginLeft: 10}}/>
+                    <input {...register("todo")} style={{marginLeft: 10}}/>
                 </label>
                 <input type="submit" value="Submit"  style={{marginLeft: 10}}/>
             </form>
             <ul>
                 {toDoData.map((item, index) => {
-                return <li key={index}><ToDo todo={item} deleteToDo={deleteToDo}/></li>
+                return <li key={index}><ToDo todo={item.todo} deleteToDo={deleteToDo}/></li>
             })}
             </ul>
         </div>
